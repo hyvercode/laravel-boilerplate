3b351e02b783028e25cbaac94dbffd165ec99904
mohirwanh <mohirwan@gmail.com>
2022-03-23 21:15:25 +0700

    refactoring

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('menu_roles')->nullable();
             $table->string('fcm_token')->nullable()->unique();
+            $table->string('personal_access_token')->nullable()->unique();
             $table->string('avatar')->nullable();
             $table->boolean('active')->nullable();
             $table->uuid('created_by');
